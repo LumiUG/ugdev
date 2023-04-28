8a601eefa609288361129b1c991944ef0dc5ab7a
xUnderGame <unaigiz1@gmail.com>
2023-04-29 00:17:08 +0200

    Modified a render and "something"

--- assets/scripts/something.js
+++ assets/scripts/something.js
@@ -1,55 +1,17 @@
 const m1 = [
 	"",
-	"Hello",
-	"'Something' is going on here.",
-	"You might wanna...",
-	"Check back later?",
+	"Been a while, crocodile-",
+	"Okay, I'm just playing with you.",
+	"Whatever was supposed to happen",
+	"has already happened >:3",
+	"So um... just enjoy yourself!",
+	"Here, take another heart!",
+	"Now leave, and have an amazing day,",
+	"because you are valid and deserve it <3",
+	"See ya around. Toodle pip!",
 	"",
 	"",
-	"There's nothing else in here for you.",
-	"This'll be 'something' in the future.",
-	"But for now, you should leave.",
-	"",
-	"",
-	"",
-	"What are you waiting for, now?",
-	"",
-	"Really, don't you have better stuff to do?",
-	"If not, I guess we are on the same boat.",
-	"Let's see...",
-	"Tell me your name.",
-	"",
-	"Right, I'm only text.",
-	"",
-	"",
-	"So...",
-	"Did you know?",
-	"I spent two hours on this.",
-	"Two hours on this little page.",
-	"Originally, I intended for it to have a 'typing' effect.",
-	"But oh well, I had to scrap that.",
-	"It's better off this way anyways.",
-	"",
-	"You know what?",
-	"Since you've been here for so long...",
-	"I'll tell you a secret:",
-	"This will (most likely) be the home for a secret project.",
-	"The 'Sanguso Project'.",
-	"",
-	"",
-	"Alright, I'm bored.",
-	"Better go do something productive!",
-	"",
-	"Oh right! Before leaving you here by yourself...",
-	"I completly forgot, sorry, haha.",
-	"I just wanted to thank you for staying.",
-	"That's about it.",
-	"See ya around!",
-	"<3",
-	"<3",
-	"<3",
-	"<3",
-	"(I mean it)"
+	"<3"
 ];
 
 var index = 0;
@@ -61,9 +23,7 @@ function sendMessage(messages) {
 		index++;
 
 		// Text actions
-		if (currMsg == "Two hours on this little page.") document.getElementsByTagName("body")[0].style.color = "red";
-		else if (currMsg == "Originally, I intended for it to have a 'typing' effect.") document.getElementsByTagName("body")[0].style.color = "white";
-		else if (currMsg == "I just wanted to thank you for staying.") document.getElementById("heart").style.display = "inline";
+		if (currMsg == "Here, take another heart!") document.getElementById("heart").style.display = "inline";
 		return
 	}
 	// Stops the setInterval.
