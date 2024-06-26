53ede5221f39e59ce0292031d1d57d4916a531bf
xUnderGame <unaigiz1@gmail.com>
2024-06-26 03:45:55 +0200

    im going to sleep ill finish this tomorrow LMFAOO

--- assets/scripts/birdup.js
+++ assets/scripts/birdup.js
@@ -5,13 +5,16 @@
 // NO SLEEP .
 
 // variables
+var favicon = document.getElementById("favicon");
 var BIRDUP = document.getElementById("BIRDUP");
 var boopArea = document.getElementById("boopArea");
+var messageField = document.getElementById("messageField");
 var counter = document.getElementById("counter");
 var clicks = 0;
 
 // setup
 boopArea.addEventListener("click", () => { ClickEvent(); })
+var moveTimer = setInterval(function () { MoveMessage(); }, 550);
 
 // haha boop click click boop hehehehehe
 function ClickEvent()
@@ -20,13 +23,25 @@ function ClickEvent()
     // initial click
     if (clicks <= 1) {
         document.body.style.backgroundImage = "url('/assets/images/artwork/lumi/wiggle.gif')";
-        document.title = "BIRD UP"
+        favicon.setAttribute("href", "/assets/images/artwork/lumi/wiggle.gif")
+        messageField.style.opacity = 1;
         BIRDUP.style.opacity = 1;
         counter.style.opacity = 1;
+        document.title = "BIRD UP"
     }
 
-    if (clicks == 10) 
+    // clicks
+    switch (clicks) {
+        case 2: messageField.textContent = "boops?"; break;
+        case 10: messageField.textContent = "keep,,"; break;
+    }
 
     // new boop !!
     counter.textContent = `Boops: ${clicks}`;
+}
+
+// WEE
+function MoveMessage() {
+    messageField.style.top = Math.random() * screen.height * 0.80 + "px";
+    messageField.style.left = Math.random() * screen.width * 0.90 + "px";
 }
