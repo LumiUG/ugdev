870eba3904789d323c58cbe93176dcb486405f3e
xUnderGame <unaigiz1@gmail.com>
2026-03-16 19:02:23 +0100

    upd

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -144,17 +144,18 @@ const randMessages = [
     "awawawawawawawa",
     "\"vcf hg\"",
     "Press F5?",
-    "Half Sun Games.....",
+    // "Half Sun Games.....",
     "Did you check out my games yet?",
     ":3",
     ":3c",
     ">:3c",
     "CSS is <span style='position: relative; top: -20px;'>hard</span>",
     "<marquee>weeeeeeee</marquee>",
-    "<img src='/assets/images/favicon.png'/>",
+    // "<img src='/assets/images/favicon.png'/>",
     "<img src='/assets/images/artwork/lumi/wiggle.gif' style='height: auto'/>",
     "Like a firebird, just burn it all away",
-    "Strong enough to forge my destiny"
+    "Strong enough to forge my destiny",
+    "Gone caving."
 ];
 
 var tooltip = document.getElementById("tooltip");
